50d14258e791a9bd509f8b6eb1934d9dea67b3bb
Tim Sullivan <timsullivan97@gmail.com>
2021-01-04 16:43:50 -0800

    launch school: add prep exercises

---
+++ preparatory_exercises/exercise_5.js
@@ -0,0 +1,6 @@
+// String.prototype.substring() - instance method
+// Object.create() - static
+// String.fromCharCode() - static
+// slice() - instance method
+// Object.prototype.toString() - instance method
+
